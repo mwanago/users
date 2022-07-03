2148fea3b2b8d08d9c6e652172bdeb5be0ce3f59
MarekKluk <105215275+MarekKluk@users.noreply.github.com>
2022-07-03 18:02:58 +0200

    small fix

--- app/functions/sortingCompaniesByAmountOfUsers.js
+++ app/functions/sortingCompaniesByAmountOfUsers.js
@@ -1,8 +1,8 @@
 
 export default function sortingCompaniesByAmountOfUsers (list) {
     list.sort((a,b) => {
-        if (a.usersOfSpecificCompany.length > b.usersOfSpecificCompany.length) { return -1; }
-        if (a.usersOfSpecificCompany.length < b.usersOfSpecificCompany.length) { return 1; }
+        if (a.usersOfSpecificCompany.length < b.usersOfSpecificCompany.length) { return -1; }
+        if (a.usersOfSpecificCompany.length > b.usersOfSpecificCompany.length) { return 1; }
         return 0;
     });
 }
--- app/index.js
+++ app/index.js
@@ -17,50 +17,6 @@ window.onload = () => {
     .then(handleDataFetched);
 };
 
-<<<<<<< HEAD
-function handleDataFetched (responses) {
-  Promise.allSettled([responses[0].value.json(), responses[1].value.json()])
-    .then(dataArray => {
-      const users = dataArray[0].value;
-      const companies = dataArray[1].value;
-      listOfUsers = creatingListOfUsers(users);
-      listOfCompanies = creatingListOfCompanies(companies);
-      pushingUsersToCompanies(listOfUsers,listOfCompanies);
-      console.log(listOfCompanies);
-    });
-}
-function pushingUsersToCompanies (users,companies) {
-  for (let i=0; i<= users.length-1; i++) {
-    const userIndex = users[i].companyNumber;
-    companies[userIndex].usersOfSpecificCompany.push(users[i].name);
-  }
-  return companies;
-}
-
-function creatingListOfUsers (usersRawData) {
-  const usersList = [];
-  for (let i = 0; i<= usersRawData.length -1; i++) {
-    usersList.push ({
-      name: usersRawData[i].name,
-      email: usersRawData[i].email,
-      company: usersRawData[i].uris.company,
-      companyNumber: usersRawData[i].uris.company.substring(11)
-    });
-  }
-  return usersList;
-}
-
-function creatingListOfCompanies (listOfCompanies) {
-  const companiesList = [];
-  for (let i = 0; i<= listOfCompanies.length -1 ; i++) {
-    companiesList.push ({
-      name: listOfCompanies[i].name,
-      usersOfSpecificCompany: []
-    });
-  }
-  return companiesList;
-}
-=======
 function handleDataFetched (responses){
     Promise.allSettled([responses[0].value.json(), responses[1].value.json()])
         .then(dataArray => {
@@ -80,5 +36,5 @@ buttonForSorting?.addEventListener("click", (event) => {
     sortingCompaniesByAmountOfUsers(listOfCompanies);
     creatingListOfCompaniesWithUsers(listOfCompanies);
 });
->>>>>>> MarekPD8
+
 
